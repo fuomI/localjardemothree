91dab4e827007e68c6586103a100e29ae9d6ef64
Joni Jansson <joni.jansson@saunalahti.fi>
2023-02-28 13:05:25 +0200

    first commit

---
+++ src/main/java/fi/jonij/localjardemothree/LocaljardemothreeApplication.java
@@ -0,0 +1,13 @@
+package fi.jonij.localjardemothree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaljardemothreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaljardemothreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/jonij/localjardemothree/LocaljardemothreeApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.jonij.localjardemothree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaljardemothreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
