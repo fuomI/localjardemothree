0dfb17bea31e1c651bb02a532415c2b96575563a
Joni Jansson <joni.jansson@saunalahti.fi>
2023-02-28 13:32:36 +0200

    Added local jar, linked local jar in pom.xml, tested in main method that local jar works

--- src/main/java/fi/jonij/localjardemothree/LocaljardemothreeApplication.java
+++ src/main/java/fi/jonij/localjardemothree/LocaljardemothreeApplication.java
@@ -2,12 +2,19 @@ package fi.jonij.localjardemothree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import fi.jonij.jar.demo.*;
 
 @SpringBootApplication
 public class LocaljardemothreeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocaljardemothreeApplication.class, args);
+		
+		// TEST IF LOCAL JAR WORKS
+		int sum = Foo.calculateSum(3, 2);
+		System.out.println("**************");
+		System.out.println(sum);
+		System.out.println("**************");
 	}
 
 }
